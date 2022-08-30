b2073644750d9db77d407b2ca40c87b76a39ee70
Ashwin Radhakrishnan <ashwin.bittu@gmail.com>
2022-08-30 15:05:29 +1000

    Add files via upload

--- variables.tf
+++ variables.tf
@@ -82,12 +82,6 @@ variable "resolver_rule_associations" {
   default     = {}
 }
 
-variable "aws_region" {
-}
-
-variable "repave_strategy" {
-}
-
 variable "app_env" {
 }
 
@@ -97,22 +91,5 @@ variable "app_name" {
 variable "app_id" {
 }
 
-variable "aws_route53_zone_name" {
-}
-
-variable "aws_route53_record_name" {
-}
-
-variable "aws_vpc_id" {
-}
-
-variable "aws_elb_dns_name" {
-}
-
-variable "aws_elb_zone_id" {
-}
-
-variable "aws_route53_zone_id" {
-}
 
 
